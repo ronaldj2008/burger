28c90cacfaaee2663de4338384356aef9c568f12
Ron J <ronaldj2008@gmail.com>
2017-05-27 13:20:21 -0700

    having trouble connecting to server

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,33 @@
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
 
-		connection = mysql.createConnection(process.env.JAWSDB_URL);
+// 		connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-	} else {
+// 	} else {
 
-			connection = mysql.createConnection({
-				port: 3306,
-				host: 'localhost',
-				user: 'root',
-				password: '',
-				database: 'burgers_db'
-			})
+// 			connection = mysql.createConnection({
+// 				port: 3306,
+// 				host: 'localhost',
+// 				user: 'root',
+// 				password: '',
+// 				database: 'burgers_db'
+// 			})
 
-	};
+// 	};
 
 
-	connection.connect(function(err) {
-		if (err) {
-			console.error('ERROR: MySQL connection error --' + err.stack + '\n\n');
-			return;
-		}
-		console.log('Connected to MySQL database as id' + connection.threadId + '\n\n');
-		});
+// 	connection.connect(function(err) {
+// 		if (err) {
+// 			console.error('ERROR: MySQL connection error --' + err.stack + '\n\n');
+// 			return;
+// 		}
+// 		console.log('Connected to MySQL database as id' + connection.threadId + '\n\n');
+// 		});
 
-module.exports = connection;
+// module.exports = connection;
 	
 	
--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 
-
-
-
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
-// var db = require("./models/todo.js");
+app.use(express.static(__dirname + '/public'));
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
-// Static directory
-app.use(express.static("./public"));
+app.use(methodOverride('_method'));
+app.engine('handlebars',exphbs ({
+	defaultLayout: 'main'
+}));
 
-// Routes =============================================================
+app.set('view engine', 'handlebars');
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+var routes = require('./controllers/routes.js');
+app.use('/', routes);
 
-// Starting our express app
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+var port = 3000;
+app.listen(port);	
