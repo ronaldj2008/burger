7a01f50aeddfa5c8a37709c7a437c157e2a6c3b4
Ron J <ronaldj2008@gmail.com>
2017-05-29 16:53:22 -0700

    Server working need Hanldebar to render

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,20 @@
 
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
-// var connection;
+var connection = mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: '',
+				database: 'burgers_db'
+			})
 
-// if (process.env.JAWSDB_URL) {
+connection.connect(function(err){
+	if(err)throw err;
+	console.log("connected as id: "+connection.threadid);
 
-// 		connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-// 	} else {
-
-// 			connection = mysql.createConnection({
-// 				port: 3306,
-// 				host: 'localhost',
-// 				user: 'root',
-// 				password: '',
-// 				database: 'burgers_db'
-// 			})
-
-// 	};
+});
 
 
-// 	connection.connect(function(err) {
-// 		if (err) {
-// 			console.error('ERROR: MySQL connection error --' + err.stack + '\n\n');
-// 			return;
-// 		}
-// 		console.log('Connected to MySQL database as id' + connection.threadId + '\n\n');
-// 		});
-
-// module.exports = connection;
+module.exports = connection;
 	
 	
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var burger = require('../models/burger.js');
 
 router.get('/', function(req,res){
-	res.render('index');
+	burger.all(function(burger_data){
+		console.log(burger_data);
+		res.render('index');
+	})
 })
+
+router.put('/burgers/update',function(req,res){
+	burger.update(req.body.burger_id, function(result){
+		res.redirect('/')
+	});
+});
 
 module.exports = router;
