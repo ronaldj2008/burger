7cfa94c344dee9d83647af78b88c011fbe475d68
Ron J <ronaldj2008@gmail.com>
2017-06-01 18:34:16 -0700

    handle bars rendering

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,7 +5,7 @@ var burger = require('../models/burger.js');
 router.get('/', function(req,res){
 	burger.all(function(burger_data){
 		console.log(burger_data);
-		res.render('index');
+		res.render('index', {burger_data: burger_data});
 	})
 })
 
@@ -13,6 +13,6 @@ router.put('/burgers/update',function(req,res){
 	burger.update(req.body.burger_id, function(result){
 		res.redirect('/')
 	});
-});
+});   
 
 module.exports = router;
