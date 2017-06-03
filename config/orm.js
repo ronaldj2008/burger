c1398dee5a93732f703d1f59d68cf894877c7355
Ron J <ronaldj2008@gmail.com>
2017-06-03 09:16:33 -0700

    almost there

--- config/orm.js
+++ config/orm.js
@@ -12,14 +12,21 @@ var orm = {
 	update: function(tableInput, condition, cb){
 		connection.query('UPDATE' +tableInput+ 'SET devoured=true WHERE id='+condition+';', function(err,result){
 			if(err)throw err;
+			cb(result);
 		})
+	},
+
+	create: function(tableInput,val,cb){
+		connection.query('INSERT INTO' +tableInput+" (burger_name) VALUES ('"+val+"');", function(err,result){
+				if(err)throw err;
+				cb(result);
+			})
 	}
 }
 
  		
 
 module.exports = orm;
-
 
 
 
--- controllers/routes.js
+++ controllers/routes.js
@@ -5,14 +5,19 @@ var burger = require('../models/burger.js');
 router.get('/', function(req,res){
 	burger.all(function(burger_data){
 		console.log(burger_data);
-		res.render('index', {burger_data: burger_data});
+		res.render('index', {burger_data});
 	})
 })
 
 router.put('/burgers/update',function(req,res){
 	burger.update(req.body.burger_id, function(result){
-		res.redirect('/')
+		res.redirect('/');
 	});
 });   
 
+router.post('/burgers/create',function(req, res){
+	burger.create(req.body.burger_name, function(res){
+		res.redirect('/');
+	})
+})
 module.exports = router;
